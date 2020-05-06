de69555a2e05fd98b23ae659d79c474f79936792
Henrik Kurkela <henrik.kurkela@gmail.com>
2020-05-06 17:14:00 +0300

    Part 4b

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,50 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    response.json(blog.toJSON())
+})
+
+blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    if (typeof blog.likes === 'undefined' || blog.likes === null) {
+        blog.likes = 0
+    }
+
+    if (typeof blog.title === 'undefined' || blog.title === null || typeof blog.url === 'undefined' || blog.url === null) {
+        response.status(400).end()
+    } else {
+        const result = await blog.save()
+        response.status(201).json(result.toJSON())
+    }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    try {
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch (error) {
+        response.status(400).end()
+    }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const blog = {
+        likes: request.body.likes
+    }
+    try {
+        const result = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        response.json(result.toJSON())
+    } catch (error) {
+        response.status(400).end()
+    }
 })
 
 module.exports = blogsRouter
---
+++ tests/blog_api.test.js
@@ -0,0 +1,116 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
+    { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 },
+    { _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12, __v: 0 },
+    { _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10, __v: 0 },
+    { _id: "5a422ba71b54a676234d17fb", title: "TDD harms architecture", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", likes: 0, __v: 0 },
+    { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(initialBlogs)
+})
+
+test('correct amount of blogs is returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('identifying field is named id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog can be added ', async () => {
+    const initialResponse = await api.get('/api/blogs')
+
+    const newBlog = {
+        title: "Full Stack",
+        author: "StackMaster",
+        url: "https://stack.com/",
+        likes: 1
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialResponse.body.length + 1)
+})
+
+test('if blog is added with no votes zero will be assumed', async () => {
+    const newBlog = {
+        title: "Half Stack",
+        author: "StackDiscipline",
+        url: "https://halfstack.com/"
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+
+    expect(response.body.likes).toBeDefined()
+})
+
+test('if blog is added with no url or title it will not be added', async () => {
+    const newBlog = {
+        author: null,
+        url: "https://stack.com/",
+        likes: 1
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+
+    expect(response.status).toBe(400)
+})
+
+test('a blog may be removed by issuing http delete request', async () => {
+    const newBlog = {
+        title: "Full Stack",
+        author: "StackMaster",
+        url: "https://stack.com/",
+        likes: 1
+    }
+
+    const result = await api
+        .post('/api/blogs')
+        .send(newBlog)
+
+    const response = await api.get(`/api/blogs/${result.body.id}`)
+    const deleteBlog = await api.delete(`/api/blogs/${result.body.id}`)
+    expect(deleteBlog.status).toBe(204)
+})
+
+test('a blog may be edited by issuing http put request', async () => {
+    const newBlog = {
+        title: "Full Stack",
+        author: "StackMaster",
+        url: "https://stack.com/",
+        likes: 1
+    }
+
+    const result = await api
+        .post('/api/blogs')
+        .send(newBlog)
+
+    newBlog.likes += 1
+
+    await api.put(`/api/blogs/${result.body.id}`).send(newBlog)
+    const newResult = await api.get(`/api/blogs/${result.body.id}`)
+    expect(newResult.body.likes).toBe(newBlog.likes)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
